3948476a7a2f89c5ae6bc791810a52f7dd081c95
root <Yoshinori.Kuwada@yokogawa.com>
2022-06-15 08:29:15 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -128,8 +128,8 @@ resource "aws_instance" "hashicat" {
 ￥
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = devops
-    Billable = true
+    Department = "devops"
+    Billable = "true"
   }
 }
 
